ff518580a95ca8992cb3007d6f0b8e6669fbaf6b
AoxLiu <64894896+AoxLiu@users.noreply.github.com>
2020-08-12 21:02:07 +0300

    Update Check_sample.sh

--- Cromwell_Affymetrix_FinnGen_sample/BatchAll/Check_sample.sh
+++ Cromwell_Affymetrix_FinnGen_sample/BatchAll/Check_sample.sh
@@ -51,26 +51,10 @@ rm ../check/batch.lst
 for t in summary snp-posteriors report confidences calls
 do
 echo ${t}
-for i in {01..51}      # assuming batch numbers are from 01 to 51, but actually there are no b32 and b34 currently due to these two batches have too less samples after removing samples with restricted consent
+for i in {01..51}      # assuming batch numbers are from 01 to 51, but actually there are no b32 and b34 currently due to these two batches have too less samples after removing samples with restricted consent (these two batches are delivered now)
 do
 ls -alh AxiomGT1_b${i}/AxiomGT1_b${i}_V2*.${t}*.txt >> ../check/batch.lst
 done
 done
 
        
-
-# Prepare sample_tsv_file
-echo "sample_id	batch_id	cel" > ../check/B0102.sample.tsv
-
-for i in b01 b02
-do
-echo $i
-join <(less AxiomGT1_${i}/AxiomGT1_${i}_V2*.report_mapped_selected.txt|grep -v '#'|grep -v cel_files|awk '{print $1}'|sort -k1,1) <(head -n 5000 AxiomGT1_${i}/AxiomGT1_${i}_V2P2.calls.mapped_selected.txt|grep cel-[1-9]|awk -F= -v b=${i} '{print $3,b,$2}'|sort -k1,1)|tr ' ' '\t' >> ../check/B0102.sample.tsv
-done
-
-wc -l ../check/B0102.sample.tsv   # 9014, with first line as title
-
-gsutil cat gs://from-fg-datateam/check/B0102.sample.tsv|awk -F"\t" '{print $1,$2,$1}'|tr ' ' '\t' > B0102_sort.sample.tsv
-gsutil cp B0102_sort.sample.tsv  gs://from-fg-datateam/check/
-
-
