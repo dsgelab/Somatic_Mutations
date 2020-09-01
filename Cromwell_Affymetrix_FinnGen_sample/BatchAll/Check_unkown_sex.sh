ef2f3fd126728c5aad538756b52c4669ac356fba
AoxLiu <64894896+AoxLiu@users.noreply.github.com>
2020-09-01 22:49:07 +0300

    Add files via upload

---
+++ Cromwell_Affymetrix_FinnGen_sample/BatchAll/Check_unkown_sex.sh
@@ -0,0 +1,24 @@
+### This script is to check FinnGen R6 mCA results from mocha pipelines
+
+cd  /Users/aoxliu/Documents/Project2_Finngen_mCA/Analysis_FinnGen_mCA/FinnGenBatchAll/Result/
+
+
+
+
+
+##========================================================================================
+######
+# there are many XXY individuals classified as unknown sex and there are individuals that are clearly mLOY that are classified as unknown sex ...
+# this is how Affymetrix Axiom classifies them ... 
+# the latter could be fixed if you want to include expanded mLOY in your analyses
+
+
+
+
+
+
+
+
+
+
+
